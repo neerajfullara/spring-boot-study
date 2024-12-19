f11e56b69d1fd8ce8f460d4a60ecb26a2341f295
neerajfullara <fullaraneeraj@gmail.com>
2024-12-19 11:07:33 +0530

    Created the project structure.

---
+++ Project/ecom-proj/src/main/java/com/neeraj/ecom_proj/model/Product.java
@@ -0,0 +1,29 @@
+package com.neeraj.ecom_proj.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    private int id;
+    private String name;
+    private String desc;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean availability;
+    private int quantity;
+}
